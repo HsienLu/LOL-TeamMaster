3353419f833258e376bdc16a936863512df8bbda
Ming <a955269@gmail.com>
2023-11-29 17:47:52 +0800

    整理 header.js 架構

---
+++ assets/js/config.js
@@ -0,0 +1,2 @@
+export const api_path = "http://localhost:3000";
+export const userIsLogin = localStorage.getItem("user");
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,44 +1,46 @@
 import axios from "axios";
 import Swal from "sweetalert2";
+import { api_path, userIsLogin } from "./config";
 
-const api_path = "http://localhost:3000";
 // const jsLoginForm = document.querySelector(".jsLoginForm");
 const jsLoginBtn = document.querySelector(".jsLoginBtn");
 
-jsLoginBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+if (!userIsLogin) {
+  jsLoginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
 
-  const userEmail = document.querySelector("#userEmail-login").value;
-  const userPassword = document.querySelector("#userPassword-login").value;
+    const userEmail = document.querySelector("#userEmail-login").value;
+    const userPassword = document.querySelector("#userPassword-login").value;
 
-  axios
-    .post(`${api_path}/login`, {
-      email: userEmail,
-      password: userPassword,
-    })
-    .then((res) => {
-      console.log(res);
-      localStorage.setItem("user", res.data.accessToken);
-      Swal.fire({
-        icon: "success",
-        title: "登入成功",
-        showConfirmButton: false,
-        timer: 2000,
-        background: "#060818",
-        color: "#D6EEFF",
+    axios
+      .post(`${api_path}/login`, {
+        email: userEmail,
+        password: userPassword,
+      })
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("user", res.data.accessToken);
+        Swal.fire({
+          icon: "success",
+          title: "登入成功",
+          showConfirmButton: false,
+          timer: 2000,
+          background: "#060818",
+          color: "#D6EEFF",
+        });
+        setTimeout(() => {
+          location.href = "index.html";
+        }, 2000);
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: "error",
+          title: error.response.data,
+          showConfirmButton: false,
+          timer: 2500,
+          background: "#060818",
+          color: "#D6EEFF",
+        });
       });
-      setTimeout(() => {
-        location.href = "index.html";
-      }, 2000);
-    })
-    .catch((error) => {
-      Swal.fire({
-        icon: "error",
-        title: error.response.data,
-        showConfirmButton: false,
-        timer: 2500,
-        background: "#060818",
-        color: "#D6EEFF",
-      });
-    });
-});
+  });
+}
