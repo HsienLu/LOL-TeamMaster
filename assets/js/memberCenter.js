54c97c2c20e16f19b02d0048f44665392083aee6
winniecat777 <winniecat777@gmail.com>
2023-12-04 02:11:35 +0800

    edit banList

--- assets/js/memberCenter.js
+++ assets/js/memberCenter.js
@@ -5,17 +5,18 @@ import { api_path, userIsLogin, memberId } from "./config";
 let data;
 let commentData = [];
 let friendListData = [];
-let blackListData = [];
+let banListData = [];
 let historicalTeamListData = [];
 let positionSelected;
 const commentList = document.querySelector('.evaluateCard');
 const friendList = document.querySelector('.friendCard');
-const blackList = document.querySelector('.blackCard');
+const banList = document.querySelector('.blackCard');
 const historicalTeamList = document.querySelector('.historicalCard');
 
 function init(){
   getCommentList();
   getFriendList();
+  getBanList();
 }
 init();
 
@@ -89,11 +90,7 @@ function renderCommentList(){
 }
 
 function getFriendList(){
-  axios.get(`${api_path}/friendLists?_expand=user`, {
-    headers: {
-      Authorization: `Bearer ${userIsLogin}`,
-    },
-  })
+  axios.get(`${api_path}/friendLists?_expand=user`)
   .then((res) => {
     data = res.data;
     for(let i = 0; i < data.length; i++) {
@@ -180,6 +177,80 @@ function renderFriendList(){
     `
   })
   friendList.innerHTML = str;
+}
+
+function getBanList(){
+  axios.get(`${api_path}/banLists?_expand=user`)
+  .then((res) => {
+    data = res.data;
+    for(let i = 0; i < data.length; i++) {
+      banListData.push(data[i].user);
+    }
+    renderBanList();
+  })
+}
+
+function renderBanList(){
+  let str = "";
+  banListData.forEach((item) => {
+    str += `
+    <div class="swiper-slide blackListCard border border-2 border-radius border-primary bg-dark blueShadow">
+      <div class="meber-card-top">
+        <div class="member-avatar position-relative w-100">
+          <div class="avatar w-100">
+            <img
+              src="../assets/images/avatar/${item.avatar}.png"
+              alt=""
+              class="member-avatar-style"
+              style="height: 300px; object-fit: cover"
+            />
+          </div>
+
+          <div class="member-avatar-style position-absolute bottom-0"></div>
+
+          <div class="metal position-absolute" style="width: 85px; height: 65px">
+            <div class="parallelogram-1">
+              <div class="parallelogram-content-avatar"
+                style="
+                  background-image: url(../assets/images/ranking/${item.userRank}.png);
+                  background-size: cover;
+                  background-position: center;
+                  width: 85px;
+                  height: 60px;
+                "></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <h5 class="mt-12 text-center mb-4">${item.username}</h5>
+      <div class="tag-group d-flex justify-content-center g-8 w-100 flex-wrap">
+        <div class="bage bage-text">TOP</div>
+        <div class="bage bage-text">JG</div>
+        <div class="bage bage-text">MID</div>
+        <div class="bage bage-text">ADC</div>
+        <div class="bage bage-text">SUP</div>
+      </div>
+      <div class="thumb my-6">
+        <p class="thum-text text-center">
+        ${item.thumb} <span><i class="fa-regular fa-thumbs-up"></i></span>
+        </p>
+      </div>
+      <div
+        class="button-all d-flex justify-content-center gap-4 mb-8"
+        style="height: 48px"
+      >
+        <button class="btn btnShadow border border-primary text-primary w-80">
+          剔除
+          <span>
+            <i class="fa-solid fa-user-slash ms-1"></i>
+          </span>
+        </button>
+      </div>
+    </div>
+    `
+  })
+  banList.innerHTML = str;
 }
 
 
