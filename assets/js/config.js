ec166d322630deed1d8eed924023fbe7b0fc1a3d
HsienLu <sjps946403@gmail.com>
2023-12-11 16:39:20 +0800

    修改API路徑

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-export const api_path = "http://localhost:3000";
+export const api_path = "https://json-server-stable.onrender.com";
 export const userIsLogin = localStorage.getItem("user");
 export const memberId = Number(localStorage.getItem("userId"));
 export const memberAvatar = localStorage.getItem("userAvatar");
