9e3ae9ac109de65f7334c2d31dbab563db0b9a65
HsienLu <sjps946403@gmail.com>
2023-12-12 21:17:20 +0800

    fix

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,4 +2,4 @@ export const api_path = "https://lol-teammaster-jasonserver.onrender.com";
 export const userIsLogin = localStorage.getItem("user");
 export const memberId = Number(localStorage.getItem("userId"));
 export const memberAvatar = localStorage.getItem("userAvatar");
-export const localhost = "https://hsienlu.github.io/";
+export const localhost = "https://hsienlu.github.io";
